c2d7eb479b28d2b737dd2217abb48f726b200dc6
JavierDS12 <javier.soican@educa.madrid.org>
2025-12-11 12:34:15 +0100

    caballo y creacion de fecha

---
+++ src/src/Caballo.java
@@ -0,0 +1,8 @@
+public class Caballo {
+    private boolean genero; //false hembra true macho
+    private int altura;
+    private int potencia;
+    private String comidaFavorita;
+    private boolean competidor;
+    private Fecha nacimiento; //
+}
---
+++ src/src/Fecha.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    public Fecha(int dia, int mes, int anyo) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anyo;
+    }
+
+    public Fecha() {
+        Random rand = new Random();
+        dia = rand.nextInt(30) + 1;
+        mes = rand.nextInt(1, 13);
+        ;
+        anio = rand.nextInt(1980, 2030);
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    /*public void setDia(int diaNuevo){
+        dia = diaNuevo;
+    }*/
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+}
