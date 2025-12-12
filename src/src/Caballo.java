2ada1f6caad8af8f52b693c653601bc60f0f3d26
JavierDS12 <javier.soican@educa.madrid.org>
2025-12-12 10:16:15 +0100

    aiudgas

--- src/src/Caballo.java
+++ src/src/Caballo.java
@@ -1,8 +1,40 @@
+import java.util.Random;
 public class Caballo {
     private boolean genero; //false hembra true macho
     private int altura;
     private int potencia;
     private String comidaFavorita;
-    private boolean competidor;
-    private Fecha nacimiento; //
+    private boolean competidor; //false no competidor true competidor
+    private Fecha nacimiento;
+    private String [] color;
+    private String nombre;
+    public Caballo(boolean genero, int altura, int potencia, String comidaFavorita, boolean competidor, Fecha nacimiento, String[] color){
+
+    }
+    public Caballo(){
+        Random random = new Random();
+        genero = random.nextBoolean();
+        altura = random.nextInt(100, 200);
+        potencia = random.nextInt();
+        comidaFavorita = Constantes.COMIDAFAVORITA[random.nextInt(Constantes.COMIDAFAVORITA.length)];
+        boolean competidor = random.nextBoolean();
+        nacimiento = new Fecha();
+        color =  new String[random.nextInt(2,3)];
+        for (int i = 0; i < color.length; i++){
+            color[i] = Constantes.COLOR[random.nextInt()];
+            for (int j = 0; j <= i; j++){
+                if (color[i].equals(color[j])){
+                    color[i] = null;
+                }
+            }
+        }
+        if (genero){
+            nombre=Constantes.MARES[random.nextInt(Constantes.MARES.length)];
+        } else {
+            nombre=Constantes.STALLIONS[random.nextInt(Constantes.STALLIONS.length)];
+        }
+    }
+    public  String toString(){
+        return (genero?"La yegua":"El caballo") + nombre;
+    }
 }
---
+++ src/src/Constantes.java
@@ -0,0 +1,6 @@
+public class Constantes {
+    public static String [] COMIDAFAVORITA = {"Zanahorias", "Paja", "Lechugas", "Césped", "Manzanas"};
+    public static String [] COLOR = {"Blanco", "Negro", "Marrón", "Gris", "Claro"};
+    public static String [] MARES = {"Almond Eye", "Gentildonna", "Vodka", "Lys Gracieux", "Haru Urara", "Mejiro McQueen"};
+    public static String [] STALLIONS =  {"Manhattan Cafe", "Agnes Tachyon", "Goldship", "Oguri Cap", "Glorious Sunday", "Sepecial Week"};
+}
